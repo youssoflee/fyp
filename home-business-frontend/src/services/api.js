c94a7e687db3a4801f1c1d5bc6fc82bfdcd8baa0
ayeshalee <62678591+ayeshalee@users.noreply.github.com>
2021-12-22 03:48:24 +0800

    headers for sanctum token

--- home-business-frontend/src/services/api.js
+++ home-business-frontend/src/services/api.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const apiClient = axios.create({
   baseURL: "http://localhost:8000",
-//   withCredentials: true,
-//   headers: {
-    // Authorization: "Bearer " + localStorage.getItem("token"),
-//   },
+  withCredentials: true,
+  headers: {
+    Authorization: "Bearer " + localStorage.getItem("token"),
+  },
 });
 
 export default apiClient;
