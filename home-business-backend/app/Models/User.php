091c1aa6606ae0fc855954f2dd89dffb57dcaede
ayeshalee <62678591+ayeshalee@users.noreply.github.com>
2022-01-09 14:46:39 +0800

    Update User.php

--- home-business-backend/app/Models/User.php
+++ home-business-backend/app/Models/User.php
@@ -48,4 +48,9 @@ class User extends Authenticatable
     {
         return $this->belongsTo(UserRole::class , 'role_id');
     }
+
+    public function Customer()
+    {
+        return $this->hasOne(Customer::class , 'user_id');
+    }
 }
